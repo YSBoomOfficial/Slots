6fdd6ad91eafdcac666772da03449afe7899da0e
Yash <63893378+YSBoomOfficial@users.noreply.github.com>
2022-01-30 03:47:01 +0000

    Deployment target now iOS 14
    Moved from App Delegate to @main struct instead

---
+++ Slots/SlotsApp.swift
@@ -0,0 +1,18 @@
+//
+//  SlotsApp.swift
+//  Slots
+//
+//  Created by Yash Shah on 16/08/2020.
+//
+
+import UIKit
+import SwiftUI
+
+@main
+struct SlotsApp: App {
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
+}
